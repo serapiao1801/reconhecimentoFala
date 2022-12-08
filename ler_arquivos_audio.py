8bd9cd71616c2ea9e2252ac2f5ae903bc8a47a6c
Sérgio Serapião <sergioserapiaod@gmail.com>
2022-12-08 17:17:22 -0300

    alteração de linguagem
    portugues para ingles

--- ler_arquivos_audio.py
+++ ler_arquivos_audio.py
@@ -1,8 +1,8 @@
 import speech_recognition as sr
-
+#este é o recognizer
 rec = sr.Recognizer()
-
+#lendo os arquivos de áudio
 with sr.AudioFile("teste.wav") as arquivo_audio:
     audio = rec.record(arquivo_audio)
-    texto = rec.recognize_google(audio, language="pt-BR")
+    texto = rec.recognize_google(audio, language="en")
     print(texto)
